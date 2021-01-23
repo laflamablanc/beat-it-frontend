d8d834d6cb84219a2d82a03c51859010828b2f91
Sean LaFlam <sean.laflam@gmail.com>
2021-01-22 19:10:17 -0500

    Update

--- beat-it-frontend/src/containers/Login.js
+++ beat-it-frontend/src/containers/Login.js
@@ -9,10 +9,19 @@ class Login extends React.Component{
         password: ""
     }
 
+    localSubmitHandler = () => {
+        this.props.history.push("/genre")
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value 
+        })
+    }
+
     render(){
         return(
-            <div className="login-page">
-                <div className="login-page-card">
+            <div className="card">
                 <img src="/LoginLogoClear.png" alt=""/>
                 <h5 className="login-page-header-text login-page-header-text-sm"> Please Login to Continue </h5>
                 <form className="login-page-form" onSubmit = {this.localSubmitHandler}>
@@ -20,11 +29,10 @@ class Login extends React.Component{
                     <input className="login-page-input" placeholder="username" type="text" id="username" name="username" onChange = {this.handleChange} value={this.state.username}/><br/>
                     <label className="login-page-input-text" for="password">Password:</label><br/>
                     <input className="login-page-input" placeholder="password" type="password" id="password" name="password" onChange = {this.handleChange} value = {this.state.password}/><br/>
-                    <input className="login-page-submit" type="submit" id="submit" name="submit"/>
+                    <input className="play-button" type="submit" id="submit" name="submit"/>
                     <br/>
                     <Link to="/signup"> Create Account Here </Link>
                 </form>
-                </div>
             </div>
         )
     }
